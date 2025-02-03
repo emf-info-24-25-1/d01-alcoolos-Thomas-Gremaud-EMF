34fce291910616d5805ead30b597cd7671b15e47
Thomas-Gremaud-EMF <thomas.gremaud@studentfr.ch>
2025-02-03 05:13:08 +0000

    devoir01 finit

--- src/app/Application.java
+++ src/app/Application.java
@@ -2,16 +2,28 @@ package app;
 
 public class Application {
     
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+        Ami ami1 = new Ami("Gabriel", 5);
+        Ami ami2 = new Ami("Lucien", 3);
+        Ami ami3 = new Ami("David", 4);
 
-}
+        
+        System.out.println(ami1.getNom() + " peut boire encore " + ami1.getBieresRestantes() + " bières.");
+        System.out.println(ami2.getNom() + " peut boire encore " + ami2.getBieresRestantes() + " bières.");
+        System.out.println(ami3.getNom() + " peut boire encore " + ami3.getBieresRestantes() + " bières.");
+
+        
+        ami1.boitUneBiere();
+        ami2.boitUneBiere();
+        ami2.boitUneBiere();
+        ami3.boitUneBiere();
+        ami3.boitUneBiere();
+
+      
+        System.out.println(ami1.getNom() + " peut encore boire " + ami1.getBieresRestantes() + " bières.");
+        System.out.println(ami2.getNom() + " peut encore boire " + ami2.getBieresRestantes() + " bières.");
+        System.out.println(ami3.getNom() + " peut encore boire " + ami3.getBieresRestantes() + " bières.");
+    }
+
+
+
+//je sais pas si mon code fonctionne, je sais pas pourquoi mais je n'ai pas le bouton run 
